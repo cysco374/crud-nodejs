199246d851f4d917452e438c3e510f9729e3def7
cysco374 <cysco374@yahoo.co.id>
2020-07-06 09:23:46 +0700

    add views and fix update query

--- index.js
+++ index.js
@@ -35,3 +35,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //set folder public
+app.use('/assets',express.static(__dirname + '/public'));
+
+//route
+app.get('/',(req, res) => {
+	let sql = "SELECT * FROM product";
+	let query = conn.query(sql, (err, results) => {
+		if(err) throw err;
+		res.render('product_view',{
+			results:results
+		});
+	});
+});
+
+//route add data
+app.post('/save',(req, res) => {
+	let data = {
+		product_name: req.body.product_name,
+		product_price: req.body.product_price
+	};
+	let sql = "INSERT INTO product SET ?";
+	let query = conn.query(sql, data,(err, results) => {
+		if(err) throw err;
+		res.redirect('/');
+	});
+});
+
+//route update data
+app.post('/update',(req, res) => {
+	let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
+	let query = conn.query(sql, (err, results) => {
+		if(err) throw err;
+		res.redirect('/');
+	});
+});
+
+//route delete data
+app.post('/delete',(req, res) => {
+	let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
+	let query = conn.query(sql, (err, results) => {
+		if (err) throw err;
+		res.redirect('/');
+	});
+});
+
+//server listen
+app.listen(8000, () => {
+	console.log('Server is running at port 8000');
+})
